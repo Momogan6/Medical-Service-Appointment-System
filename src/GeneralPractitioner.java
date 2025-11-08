0262809af5a2dda5e72e6582bf1b8f381262550b
陈浩强 <438852526@qq.com>
2025-11-08 16:16:49 +0800

    Implement subclasses for general practitioners and pediatricians, expand unique attributes and rewrite methods

--- src/GeneralPractitioner.java
+++ src/GeneralPractitioner.java
@@ -1,2 +1,35 @@
-public class GeneralPractitioner {
+public class GeneralPractitioner extends HealthProfessional {
+    private boolean provides24hService;
+
+    public GeneralPractitioner() {
+        super();
+        this.provides24hService = false;
+    }
+
+    /**
+     * @param id Doctor ID
+     * @param name Name
+     * @param field Professional field (fixed as "general practice")
+     * @param provides24hService Do you provide 24-hour service
+     */
+    public GeneralPractitioner(int id, String name, String field, boolean provides24hService) {
+        super(id, name, field);
+        this.provides24hService = provides24hService;
+    }
+
+    @Override
+    public void printDetails() {
+        System.out.println("=== Health Professional Type: General Practitioner ===");
+        super.printDetails();
+        System.out.println("Provides 24-hour Emergency Service: " + (provides24hService ? "Yes" : "No"));
+        System.out.println("======================================================");
+    }
+
+    public boolean isProvides24hService() {
+        return provides24hService;
+    }
+
+    public void setProvides24hService(boolean provides24hService) {
+        this.provides24hService = provides24hService;
+    }
 }
--- src/Pediatrician.java
+++ src/Pediatrician.java
@@ -1,2 +1,35 @@
-public class Pediatrician {
+public class Pediatrician extends HealthProfessional {
+    private String specializedAgeGroup;
+
+    public Pediatrician() {
+        super();
+        this.specializedAgeGroup = "All Ages";
+    }
+
+    /**
+     * @param id Doctor ID
+     * @param name Name
+     * @param field Professional field (fixed as "Pediatrics")
+     * @param specializedAgeGroup Proficient in age groups
+     */
+    public Pediatrician(int id, String name, String field, String specializedAgeGroup) {
+        super(id, name, field);
+        this.specializedAgeGroup = specializedAgeGroup;
+    }
+
+    @Override
+    public void printDetails() {
+        System.out.println("=== Health Professional Type: Pediatrician ===");
+        super.printDetails();
+        System.out.println("Specialized Age Group: " + specializedAgeGroup);
+        System.out.println("=============================================");
+    }
+
+    public String getSpecializedAgeGroup() {
+        return specializedAgeGroup;
+    }
+
+    public void setSpecializedAgeGroup(String specializedAgeGroup) {
+        this.specializedAgeGroup = specializedAgeGroup;
+    }
 }
