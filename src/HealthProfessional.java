cef3c05757172fd6944754b727a19c4e741a1841
陈浩强 <438852526@qq.com>
2025-11-08 14:50:39 +0800

    Initialize the project structure, create class frameworks such as HealthProfessional and GeneralPractitioner, and implement a base class for health professionals, including constructors and printDetails methods

--- src/HealthProfessional.java
+++ src/HealthProfessional.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
-
 public class HealthProfessional {
+    private int id;
+    private String name;
+    private String field;
+
+    public HealthProfessional() {
+        this.id = 0;
+        this.name = "Unknown";
+        this.field = "Unspecified";
+    }
+
+    /**
+     * @param id Health Professional ID
+     * @param name Name
+     * @param field Professional field
+     */
+    public HealthProfessional(int id, String name, String field) {
+        this.id = id;
+        this.name = name;
+        this.field = field;
+    }
+
+    public void printDetails() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Professional Field: " + field);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
 }
